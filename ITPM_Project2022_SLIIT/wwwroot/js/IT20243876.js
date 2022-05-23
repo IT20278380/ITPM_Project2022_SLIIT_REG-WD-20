deafaf58904e0149be8bcb994b06389655d2e104
it20278380 <it20278380@my.sliit.lk>
2022-05-23 13:01:14 +0530

    Update Food Validation

---
+++ ITPM_Project2022_SLIIT/wwwroot/js/IT20243876.js
@@ -0,0 +1,16 @@
+﻿function Food(event) {
+
+    var price = document.getElementById('Price').value;
+
+    if (!price(price)) {
+        document.getElementById('price_error').classList.remove('hidden');
+        event.preventDefault();
+    } else {
+        document.getElementById('price_error').classList.add('hidden');
+    }
+}
+
+function price(input_str) {
+    var re = /^[0-9]+$/;
+    return re.test(input_str);
+}
